baf3f76048d4d42fe3c32232d45748c003dcb8f1
vovve <vovve73@icloud.com>
2020-06-02 12:28:11 +0200

    Backend with CRUD

--- Backend/app.js
+++ Backend/app.js
@@ -17,4 +17,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+//Hur man gör en post
+app.post('/....', (req, res) =>{
+    console.log(req.body.name);
+    res.send("Tagit emot en POST");
+ })
+ //Hur man gör en put
+ app.put('/....../:.....Id', (req, res) =>{
+    console.log(req.params._______Id);
+    return res.send("Tagit emot en PUT med id " + req.params._______Id + " data!");
+ })
+ //Hur man gör en delete
+ app.delete('/....../:.....Id', (req, res) =>{
+    console.log(req.params._______Id);
+    return res.send("Delete av id " + req.params._______Id + " data!");
+ }) 
+
 module.exports = app;
--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,9 +1,61 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
-/* GET users listing. */
+// GET users list.
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
+  var users;
+  fs.readFile("Users.json", (err, data) =>{
+    if (err) throw err;
+    users = JSON.parse(data);
+    console.log(users);
+  })
+  res.send(users);
+ });
+  
+ // GET allusers list.
+ router.get('/allusers', function(req, res, next) {
+  var users;
+  fs.readFile("users.json", (err, data) =>{
+    if (err) throw err;
+    users = JSON.parse(data);
+    console.log(users);
+  })
+  res.send(users);
+ });
+  
+ // GET admin list.
+ router.get('/admin', function(req, res, next) {
+  var admin;
+  fs.readFile("admin.json", (err, data) =>{
+    if (err) throw err;
+    admin = JSON.parse(data);
+    console.log(admin);
+  })
+  res.send(admin);
+ });
+  
+ router.post('/login', function(req, res, next){
+  console.log(req.body);
+   var tryAdmin = req.body;
+  console.log("Try Admin ", tryAdmin.name);
+   //var currentAdmins;
+  fs.readFile("admin.json", (err, data) =>{
+    var loginResult = "Testal logga in: "
+    if (err) throw err;
+    var admin = JSON.parse(data);
+    console.log("admin ", admin);
+    var findadmin = admin.filter(a => a.name == req.body.name);
+    console.log("findadmin ", findadmin);
+   
+    console.log(findadmin[0].password);
+    console.log(req.body.password);
+   
+    if(findadmin[0].password == req.body.password){
+      console.log("Lösenordet stämmer");
+      loginResult += "success";
+    };
+  }) 
+ })
+ 
 module.exports = router;
